fac088cfd5ebde1d7106706cb4779c50aa538310
Rahul Rajendara Nakade <nakaderahul94@gmail.com>
2023-02-25 16:10:36 +0530

    This is 1st python string example

---
+++ example1_string.py
@@ -0,0 +1,13 @@
+#Write a Python program that takes a string as input and prints the length of the string.
+
+#str1=input("enter the string :")
+#b=len(str1)
+#print("Your entered string is :" + str1)
+#print("Length of enterd string is " + str (b) )
+
+
+
+str1=input("enter the string :")
+b=len(str1)
+print("Your entered string is :" + str1)
+print("Length of enterd string is ", b )
