5969ffc3be8f6aa196e91c7f066a13f83f453e19
nakaderahul94 <nakaderahul94@gmail.com>
2023-02-25 18:24:46 +0530

    Adding 2nd example of the string

---
+++ day3_string/example2_string.py
@@ -0,0 +1,4 @@
+#Write a Python program that takes a string as input and prints the first and last characters of the string.
+var1=input("Enter the string :")
+print("1st letter of the given string is :" + var1[0])
+print("last letter of the given string is :" + var1[-1])
